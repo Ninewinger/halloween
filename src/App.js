6963001dd641da891e816976394420fd91d7b7af
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2021-10-18 22:18:47 -0300

    orden

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <div>halloween</div>
+        <div>imagen</div>
+      </div>
+      <div>
+        imagen kthullu
+      </div>
+      <div>
+        <div>card</div>
+        <div>card</div>
+        <div>card</div>
+      </div>
+      <div>
+        ficha
+      </div>
+      <div>
+        footer
+      </div>
     </div>
   );
 }
