f70c8dcf42f2e0c13f6897e44b77bb84e2b16d92
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-03-10 15:05:12 -0300

    daaa

--- src/App.js
+++ src/App.js
@@ -7,33 +7,7 @@ import Carousel from 'react-bootstrap/Carousel'
 /* halloween themed page made with bootstrap with a carousel, nabvar and halloween images all centered with a black background with halloween ornaments*/
 function App() {
   return (
-    <>
-      <div className='header'>
-        <Navbar />
-      </div>
-      <div className='body mx-5'>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              src='https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2020/05/song-horror-1951607.jpg?itok=3umtQlNm'
-              className="d-block w-100 img-fluid"                      
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src='https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1616717476558-DN47GCULM2O6AFQNXOME/ghoulish-trailer-for-the-new-horror-movie-separation.jpg'
-              className="d-block w-100 img-fluid"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              src='https://i.pinimg.com/474x/80/c6/81/80c681e17a3a2fe7d5a81c6b7fca52b1.jpg'
-              className="d-block w-100 img-fluid"
-              
-            />
-          </Carousel.Item>
-        </Carousel>
-      </div>     
+    <>   
     </>
   );
 }
