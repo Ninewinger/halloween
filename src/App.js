7db587eb2aae8fc1ab5e919df2c1e760c6db7da3
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2022-03-11 13:03:52 -0300

    nose

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Carousel from 'react-bootstrap/Carousel'
 /* halloween themed page made with bootstrap with a carousel, nabvar and halloween images all centered with a black background with halloween ornaments*/
 function App() {
   return (
-    <>   
+    <> <Navbar />
+      <div id="presentacion" className="container-fluid"></div>
+      <div id="cards" className="container-fluid"></div>
+      <div id="comentarios"></div>
+      <div id="footer"></div>
+
     </>
   );
 }
